a811a44812fb72fccfe6375ccbb50ba0d34ced63
AleexanderrJ <alexander.johansson@student.ksgyf.se>
2025-12-01 07:13:08 +0100

    Update inlamning1.sql

--- inlamning1.sql
+++ inlamning1.sql
@@ -20,7 +20,7 @@ CREATE TABLE Bocker (
     Forfattare VARCHAR(100) NOT NULL,
     Genre VARCHAR(50) NOT NULL,
     Titel VARCHAR(50) NOT NULL,
-    Pris DECIMAL(10,2) NOT NULL CHECK (Pris > 0), 
+    Pris DECIMAL(10,2) NOT NULL CHECK (Pris > 0),  -- CHECK gör så att värdet måste vara större än 0.
     Lagerstatus INT NOT NULL
 );
 
@@ -28,7 +28,7 @@ CREATE TABLE Bocker (
 CREATE TABLE Bestallningar ( 
 	Ordernummer INT AUTO_INCREMENT PRIMARY KEY,
     KundID INT NOT NULL,
-    Totalbelopp DECIMAL(10,2) NOT NULL CHECK (Totalbelopp > 0),
+    Totalbelopp DECIMAL(10,2) NOT NULL CHECK (Totalbelopp > 0), -- CHECK gör så att värdet måste vara större än 0.
     Datum TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
     FOREIGN KEY (KundID) REFERENCES Kunder(KundID) -- Lånar primärnyckel från KundID i Kunder-tabellen
 );
@@ -44,33 +44,54 @@ CREATE TABLE Orderrader (
     FOREIGN KEY (ISBN) REFERENCES Bocker(ISBN) -- Lånar primärnyckel från ISBN i Böcker-tabellen 
 );
 
--- Infogar data i kunder-tabellen
+-- Infogar data i Kunder-tabellen
 INSERT INTO Kunder (Namn, Epost, Telefon, Adress) VALUES 
     ('Paul Atreides', 'paul.atreides@dune.com', '123', 'Arrakis'),
     ('Duncan Idaho', 'duncan.idaho@dune.com', '456', 'Arrakis'),
     ('Glossu Rabban', 'glossu.rabban@dune.com', '789', 'Arrakis');
 
+-- infogar data i Böcker-tabellen
+INSERT INTO Bocker (ISBN, Forfattare, Genre, Titel, Pris, Lagerstatus) VALUES 
+    ('9834032234', 'Frank Herbert', 'Sci-fi', 'Dune', 79.99, 10), 
+    ('9634982340', 'Ravrek al-Dahim', 'Sci-fi', 'Chronicles of the Spice Horizon', 99.99, 3),
+    ('9324234311', 'Brian Herbet', 'Sci-fi', 'Dune: House Atreides', 119.99, 5);
 
-INSERT INTO Bocker (ISBN, Titel, Pris, Genre, Forfattare, Lagerstatus) VALUES -- Infogar data i böcker-tabellen
-    ('9834032234', 'Dune', 199.99, 'Sci-fi', 'Frank Herbert', 10), 
-    ('9634982340', 'Chronicles of the Spice Horizon', 149.99, 'Sci-fi', 'Ravrek al-Dahim', 3),
-    ('9324234311', 'Dune: House Atreides', 199.99, 'Sci-fi', 'Brian Herbet', 5);
+-- infogar data i Orderrader-tabellen
+INSERT INTO Orderrader (ISBN, Ordernummer, Antal) VALUES
+    (9834032234, 1, 1),  
+    (9634982340, 2, 1),   
+    (9324234311, 3, 2),
+
+
+-- infogat data i Beställningar-tabellen
+INSERT INTO Bestallningar (KundID, Totalbelopp) VALUES
+    (1, 79.99),     
+    (2, 99.99),
+    (3, 199.98);    
+
+	
 
 -- För att få fram resultat visuellt från tabellerna.
 
 SELECT * FROM Kunder; -- Hämtar data från Kunder-tabellen.
+SELECT * FROM Bocker; -- Hämtar data från Böcker-tabellen.
+SELECT * FROM Orderrader; -- Hämtar data från Orderrader-tabellen.
+SELECT * FROM Bestallningar; -- Hämtar data från Bestallningar-tabellen.
+
 
 SELECT * FROM Kunder WHERE Namn = 'Paul Atreides';  -- Hämtar endast specifik namndata från Kunder-tabellen .
-
-SELECT * FROM Bocker WHERE Pris > 100; -- Hämtar endast produkter över 100kr från Produkter-tabellen .
-
-SELECT * FROM Bocker ORDER BY Pris DESC; -- Hämtar data med sorterat pris från Produkter-tabellen
+SELECT * FROM Bocker WHERE Pris > 100; -- Hämtar endast böcker som kostar över 100kr från Böcker-tabellen .
+SELECT * FROM Bocker ORDER BY Pris DESC; -- Hämtar data med sorterat pris från Böcker-tabellen
+SELECT Ordernummer FROM Orderrader WHERE Antal > 1; -- Hämtar alla ordernummer där det beställts fler än 1 exemplar av en bok
+SELECT * FROM Bestallningar WHERE Totalbelopp > 100;  -- Hämtar alla bestallningar där totalbeloppet är mer än 100kr
 
 
 /*
 Där det står NOT NULL måste det stå något. 
+Där det står CHECK så måste värdet vara större än 0
 Primary key samt UNIQUE NOT NULL är unika värden som MÅSTE vara unika.
 ISBN är som böckernas "personnummer" är unikt och ändras inte. 
+
 
 När man exekverar raderna 1-54 så skapas databasen, tabellerna + attributerna samt lite data infogas även i tabellerna.
 Jag har adderat några SELECT FROM så att man kan visa resultat av tabellerna rent visuellt.
